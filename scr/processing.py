53d36b55f63a709d10f936832883793ba5d48d3c
Лев Верёвкин <jdjfh8w@gmail.com>
2024-12-19 18:05:30 +0300

    исправка

--- scr/processing.py
+++ scr/processing.py
@@ -7,5 +7,3 @@ def filter_by_state(list_, state_='EXECUTED', prav=[]):
 
 def sort_by_date(list_):
     return sorted(list_, key=lambda x: x['date'][:10], reverse=True)
-print(sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
-))
